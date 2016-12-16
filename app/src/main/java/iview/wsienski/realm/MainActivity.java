11ab7f11bd17202e653e7d0057fbd82e9d0cdedd
wsienski <wsienski@ospartner.pl>
2016-12-16 23:29:06 +0100

    implement mechanism for creating dog and informations

--- app/src/main/java/iview/wsienski/realm/MainActivity.java
+++ app/src/main/java/iview/wsienski/realm/MainActivity.java
@@ -2,35 +2,94 @@ package iview.wsienski.realm;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.realm.Realm;
+import io.realm.Sort;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_dog_number)
+    TextView dogNumber;
+    @BindView(R.id.new_name)
+    TextView newName;
+    @BindView(R.id.new_age)
+    TextView newAge;
+    @BindView(R.id.the_oldest_dog_name)
+    TextView theOldestDogName;
+    @BindView(R.id.the_oldest_dog_age)
+    TextView theOldestDogAge;
+
+    Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView textView = (TextView) findViewById(R.id.textView);
-
-        Dog dog = new Dog();
-        dog.setName("Rex");
-        dog.setAge(1);
-
+        ButterKnife.bind(this);
         Realm.init(this);
-        Realm realm = Realm.getDefaultInstance();
+        realm = Realm.getDefaultInstance();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateInfo();
+    }
+
+    @OnClick(R.id.btn_add_dog)
+    public void addDog(View view) {
+        Dog dog = new Dog();
+        String name = newName.getText().toString();
+        Integer age = Integer.valueOf(newAge.getText().toString());
+        dog.setName(name);
+        dog.setAge(age);
+        Timber.d("addDog: name="+name+" age="+age);
+        saveDogInDB(dog);
+        updateInfo();
+    }
+
+    void saveDogInDB(Dog dog){
         realm.beginTransaction();
         realm.copyToRealm(dog);
         realm.commitTransaction();
-
-        Dog dogFromDB = realm.where(Dog.class).greaterThan("age", 0).findFirst();
-        int size = realm.where(Dog.class).findAll().size();
-        textView.setText(dogFromDB.getName() + " size=" +size);
-
-
     }
 
+    void updateInfo(){
+        long number = realm.where(Dog.class).count();
+        setDogNumber(number);
+
+        if(number>0) {
+            updateDogInfo();
+        }
+    }
+
+    private void updateDogInfo() {
+        Dog dogFromDB = realm.where(Dog.class).findAllSorted("age", Sort.DESCENDING).first();
+        Timber.d("updateDogInfo: name="+dogFromDB.getName()+" age="+dogFromDB.getAge());
+        setTheOldestDog(dogFromDB.getName(), dogFromDB.getAge());
+    }
+
+    public void setDogNumber(long number) {
+        dogNumber.setText(String.valueOf(number));
+    }
+
+    public void setTheOldestDog(String name, int age){
+        theOldestDogName.setText(name);
+        theOldestDogAge.setText(String.valueOf(age));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.close();
+    }
 }
